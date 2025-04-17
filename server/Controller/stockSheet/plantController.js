1536b75b2b84220a1821c4aa0cb58894c2551296
jaidev <jaidevsairwa@gmail.com>
2025-04-17 15:39:05 +0530

    bug-fix:plant data

--- server/Controller/stockSheet/plantController.js
+++ server/Controller/stockSheet/plantController.js
@@ -24,15 +24,13 @@ const plantFormData = async (req, res) => {
       const text = await resu.text();
       const jsonText = text.replace(/^.*?\(/, "").slice(0, -2).replace(/\/\*.*?\*\//g,"").replace(/google.visualization.Query.setResponse\(/, "");
       const data = JSON.parse(jsonText);
-      // const rows = data.table.rows.map(row => row.c.map(cell => (cell ? cell.v : null)));
-      // const filteredData = rows.filter(row => !row.includes(0));
-      console.log(data);
-      
+      const rows = data.table.rows.map(row => row.c.map(cell => (cell ? cell.v : null)));
+      const filteredData = rows.filter(row => !row.includes(0));
 
-      // res.send(filteredData);
+      res.send(filteredData);
     }
   catch(e){
-    console.log(e);
+    console.log("data not found");
     
     res.send("data not found");
   }
